72454fe97df5cba431d812dbf2ce174943e7c365
parkdaheen <daheen19@naver.com>
2022-10-25 15:39:34 +0900

    step04에 MainClass01 예제 추가함

--- Step04_Method/src/test/main/MainClass01.java
+++ Step04_Method/src/test/main/MainClass01.java
@@ -1,5 +1,6 @@
 package test.main;
 
+import test.mypac.Car;
 import test.mypac.MyObject;
 
 public class MainClass01 {
@@ -7,11 +8,31 @@ public class MainClass01 {
 		/*MyObject 객체를 하나만 생성해서 그 객체가 가지고 있는
 		walk(), getNumber(), getGreeting() 메소드를 차례로 호출해 보세요.
 		단, 메소드가 어떤 data를 리턴하면 해당 데이터를 지역변수에 담으세요.*/
-		MyObject a = new MyObject();
+		MyObject mo = new MyObject();
 		
-		a.walk();
-		int num = a.getNumber();
-		String greet = a.getGreeting();
+		mo.walk();
+		int num = mo.getNumber();
+		String greet = mo.getGreeting();
+		Car car=mo.getCar();
 		
+		//setNum(), setName(), useCar() 메소드도 호출해 보세요.
+		//메소드 호출하면서 인자로 전달할 데이터를 즉석에서 만들어서 전달하기
+		
+		mo.setNum(1);
+		mo.setName("박다흰");
+		mo.useCar(new Car());
+		
+		//메소드 호출하면서 인자로 전달할 값이 지역변수에 이미 존재하는 경우라고 가정
+		int a = 999;
+		String b = "누구게";
+		Car c1 = new Car();
+		
+		//전달할 값이 들어있는 변수명으로 전달할 수도 있다.
+		mo.setNum(a);
+		mo.setName(b);
+		mo.useCar(c1);
+		
+		//doSomething()메소드를 호출해보세요.
+		mo.doSomething(2, "dk", new Car());
 	}
 }
--- Step04_Method/src/test/mypac/MyObject.java
+++ Step04_Method/src/test/mypac/MyObject.java
@@ -20,5 +20,26 @@ public class MyObject {
 	public String getGreeting() {
 		return "안녕하세요";
 	}
+	//Car type을 리턴해주는 메소드
+	public Car getCar() {
+		return new Car();
+	}
+	
+	//int type을 메소드의 인자로 전달받는 메소드
+	public void setNum(int num) {
+		System.out.println("num"+num);
+	}
+	//String type을 메소드의 인자로 전달받는 메소드
+	public void setName(String name) {
+		System.out.println("name:"+name);
+	}
+	//Car type을 메소드의 인자로 전달받는 메소드
+	public void useCar(Car car) {
+		car.drive();
+	}
+	
+	public void doSomething(int num, String msg, Car c) {
+		System.out.println("무언가를 작업합니다.");
+	}
 
 }
