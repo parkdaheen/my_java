8bc937cec62e943e1fb1f89661bd3843a6b423e8
parkdaheen <daheen19@naver.com>
2022-10-25 11:07:06 +0900

    git commit

---
+++ Hello/src/test/main/MainClass2.java
@@ -0,0 +1,21 @@
+package test.main;
+/*
+ * 여러줄 주석입니다.
+ * 하나
+ * 두울
+ * 세엣
+ * 
+ */
+
+//공개된(모든 패키지에서 사용할 수 있는) 클래스, 클래스명은 MainClass2
+public class MainClass2 {//클래스의 시작
+	//한줄주석입니다.
+	
+	/*
+	 * 메소드명 : main
+	 * run 했을때 실행이 시작되는 아주 특별한 메소드
+	 */
+	 public static void main(String[] args) {
+		
+	}
+}//클래스의 끝
